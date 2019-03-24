da4a7368c6f24f6c2cb79b26c5d5c1ca563d204b
jman793 <31754422+jman793@users.noreply.github.com>
2019-03-23 21:57:53 -0500

    Got GUI working completely, now there is no need for console interaction

--- src/automatedFileSystem.py
+++ src/automatedFileSystem.py
@@ -1,19 +1,13 @@
 from Tkinter import *
-import os
+import os,tkFileDialog
 #Change this name to what you want the flags file to be set to
 flagFileName='flags.txt'
 #This character determines how the file path will actually be saved
 #If you do not want a delimitter than change the value to ''
 delimitingChar=':'
-lines=[]
-flags=[]
-loopcontrol=True
-possibleInputs=['yes','no','Yes','No','Y','N','y','n']
-yes=['yes','Yes','Y','y']
-no=['no','No','N','n']
 map={}
-window=Tk()
-frame=Frame(master=window,width=500,height=500)
+mainWindow=Tk()
+frame=Frame(master=mainWindow,width=500,height=500)
 frame.pack()
 
 
@@ -25,12 +19,14 @@ class ButtonFormat:
         self.b.pack_propagate(0)
 
     def buttonHandle(self):
-        print self.b.cget('text')
+        fileBrowswer=tkFileDialog.asksaveasfile(initialdir=map[self.b['text']],mode="w+")
 
 
 
 
 def readInput():
+    lines=[]
+    flags=[]
     f=open(flagFileName)
     for x in f:
         lines.append(x.split(delimitingChar,1)[-1])
@@ -40,33 +36,17 @@ def readInput():
     for i in range(len(lines)):
         map[flags[i]]=lines[i]
     setupUI()
-#TODO
+
 def setupUI():
     label=Label(master=frame,text='Where would you like the file to go?')
     label.pack()
-    for x in flags:
+    for x in map.keys():
         myButtonFormat=ButtonFormat(x)
     doneButton=Button(master=frame,text='Done',command=done)
     doneButton.pack()
 
 def done():
-    window.destroy()
-
-#TODO reformat this to work with the UI
-def moveFiles(choice):
-    path=map[choice]
-    os.chdir(path)
-    print 'Would you like to make a new folder for this addition?'
-    input=raw_input()
-    while input not in possibleInputs:
-        print 'Error incorrect input, choose from list'
-        input=raw_input()
-    if input in yes:
-        dirName=raw_input('What would you like the directory to be named?')
-        os.mkdir(dirName)
-        if raw_input('Would you like to add a file to this directory?') in no:
-            return
-    open(raw_input('What would you like to name the file? (add ext)'),"w+")
+    mainWindow.destroy()
 
 readInput()
-window.mainloop()
+mainWindow.mainloop()
